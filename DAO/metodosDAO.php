b569eb79336bde02538c7b8a237107c03e019f50
Anthony Villegas <advillegas@uees.edu.ec>
2019-07-10 20:03:42 -0500

    Este es el primer commit

---
+++ DAO/metodosDAO.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * Dao Significa Data Acces Object
+ */
+
+include '../util/ConexionDB.php';
+
+class metodosDAO {
+
+    public function ListarProductos() {
+        $cnx = new ConexionDB();
+        $cn = $cnx->getConexion();
+
+        $res = $cn->prepare("Select * from productos");
+        $res->execute();
+
+        /*
+         * Lo que hace el for each es listar cada uno de
+         * los productos de la base de datos dentro de un arreglo
+         */
+        foreach ($res as $row) {
+            $lista[] = $row;
+        }
+        return $lista;
+    }
+
+}
---
+++ util/ConexionDB.php
@@ -0,0 +1,10 @@
+<?php
+
+class ConexionDB {
+
+    public function getConexion() {
+        $cnx = new PDO("mysql:host=localhost;dbname=techec", "root", "");
+        return $cnx;
+    }
+
+}
