c7661ff3884fdc4170dff99e8d2a859a3bc715ac
Anthony Villegas <advillegas@uees.edu.ec>
2019-07-13 11:48:09 -0500

    Casi se termina la seccion de productos

--- Vistas/footer.php
+++ Vistas/footer.php
@@ -10,9 +10,9 @@
                     <div class="footer_logo"><a href="#">Tech EC</a></div>
                     <div class="copyright ml-auto mr-auto">
                         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        Copyright &copy;<script>
+                        Copyright &copy; <script>
                             document.write(new Date().getFullYear());
-                        </script> Todos los derechos reservados <a href="https://twitter.com/DanielGD40" target="_blank">Tech EC</a>
+                        </script> Todos los derechos reservados <a href="https://twitter.com/DanielGD40" target="_blank">DanielGD20</a> & <a href="https://twitter.com/Daniel_Reyna10" target="_blank">Daniel_Reyna10</a>
                         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     </div>
                     <div class="footer_social ml-lg-auto">
