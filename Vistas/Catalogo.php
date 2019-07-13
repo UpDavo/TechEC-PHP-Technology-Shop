41baa9b2b6f11f7b72653a2974eff43f21556f59
Anthony Villegas <advillegas@uees.edu.ec>
2019-07-12 20:46:48 -0500

    Cambios dentro del catalogo de productos,bootstrap

--- Vistas/Catalogo.php
+++ Vistas/Catalogo.php
@@ -8,29 +8,52 @@ $lista = $_SESSION['lista'];
 <html>
 
 <head>
-    <meta charset="UTF-8">
-    <title></title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>TechEC</title>
 </head>
 
 <body>
-    <h2 align="center">CATALOGO DE PRODUCTOS</h2>
-    <hr>
-    <br>
-    <table border="0" width="700" align="center">
-        <?php
-        $numeroObjetos = 0;
-        foreach ($lista as $recorrido) {
-            if ($numeroObjetos == 3) {
-                echo "<tr>";
-                $numeroObjetos = 1;
-            } else {
-                $numeroObjetos++;
-            }
-            ?>
-            <th><img src="../img/<?php echo $recorrido[6]; ?>" width="120" height="120" alt="Imagen">
-            <?php
-            }
-            ?>
+    <div class="container">
+
+        <h2 align="center" style="margin-top: 7%;">CATALOGO DE PRODUCTOS</h2>
+        <hr>
+        <br>
+        <table border="0" width="700" align="center" class="table">
+            <tr align="center">
+                <?php
+                $numeroObjetos = 0;
+                foreach ($lista as $recorrido) {
+                    if ($numeroObjetos == 3) {
+                        echo "<tr align='center'>";
+                        $numeroObjetos = 1;
+                    } else {
+                        $numeroObjetos++;
+                    }
+                    ?>
+                    <th>
+                        <img src="../img/<?php echo $recorrido[6]; ?>" width="120" height="120" alt="Imagen">
+                        <br>
+                        <br>
+                        <button type="button" class="btn btn-primary">Agregar</button>
+                    </th>
+
+                <?php
+                }
+                ?>
+    </div>
+
+
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
