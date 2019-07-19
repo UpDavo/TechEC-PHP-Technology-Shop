f37562f19cb89adb6892659720e7c4d65ecfafcb
Anthony Villegas <danielgd20@Anthonys-iMac.local>
2019-07-19 18:37:27 -0500

    Cambios ahora si si final en archivos

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <script>
+        function ingresar() {
+            location.href = "DAO/tiendaDAO.php?op=1";
+        }
+    </script>
+</head>
+
+<body onload="ingresar();">
+    <?php
+    // put your code here
+    ?>
+</body>
+
+</html>
