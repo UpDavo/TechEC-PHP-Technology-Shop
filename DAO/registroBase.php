ac97db21615fdbe82a353c6783fbe9f290605b5b
Anthony Villegas <danielgd20@Anthonys-iMac.local>
2019-07-19 18:52:52 -0500

    Comits antes del examen, hice lo que pude xd

--- DAO/registroBase.php
+++ DAO/registroBase.php
@@ -1,13 +1,13 @@
 <?php
 
-include '../DAO/metodosDAO.php';
+include '../util/ConexionDB.php';
 
 //Guardado de datows dentro de las variables del formulario
-$nombre = $_POST[nombre];
-$apellido = $_POST[apellido];
-$nickname = $_POST[nick];
-$email = $_POST[mail];
-$password = $_POST[clave];
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$nickname = $_POST["nick"];
+$email = $_POST["mail"];
+$password = $_POST["clave"];
 
 //conexion a la base de datos
 $cnx = new ConexionDB();
---
+++ assets/js/aplicacion_login.js
@@ -0,0 +1,6 @@
+$(document).on("submit", "FORM", function(event) {
+  event.preventDefault();
+  console.log("Formulario Enviado");
+});
+
+//Pilas ese tutorial https://www.youtube.com/watch?v=pp3_91FzLb8
