544112eeaf95da30d5b7ad7c7db23547a3bb6f6d
Anthony Villegas <advillegas@uees.edu.ec>
2019-08-05 12:15:08 -0500

    Cambios de base de datos

--- DAO/ConexionDB.php
+++ DAO/ConexionDB.php
@@ -5,7 +5,7 @@ class ConexionDB
 
     public function getConexion()
     {
-        $cnx = new PDO("mysql:host=localhost;dbname=techec2", "root", "");
+        $cnx = new PDO("pgsql:host=ec2-174-129-229-106.compute-1.amazonaws.com;dbname=d39kvmuni3s6bj", "ymqellhuegmcib", "d1da0964e0461ce04ef40fcb6179857f83a1ad42c30446c21face9e93565b454");
         return $cnx;
     }
 
